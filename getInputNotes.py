4c39a25ed5cb65eb5fb4db7f54a2bfe4d3d51973
Haley-Jang <43661515+Haley-Jang@users.noreply.github.com>
2018-11-02 09:56:14 -0400

    Update getInputNotes.py

--- getInputNotes.py
+++ getInputNotes.py
@@ -1,37 +1,36 @@
 '''
     Author: Hyeonae Jang
     Last modified: 11/01/2018
-    This moudle is to generate pasrsed form of notes and chord of seed(brainwave) music
+    This moudle is to generate parsed form of notes and chord of music(midi file)
     '''
 
 import pickle
 from music21 import converter, instrument, note, chord
 
-def get_input_notes():
+def midi_to_notes():
     notes=[]
-
-    file="Brainwave_channal1_SR500.mid"
+    
+    file="Brainwave_channal1_SR500.mid" #"Bymyself.mid"
     midi = converter.parse(file)
     # excerpt from a midi file that has been read using Music21
     midi.show('text')
-
+    
     print("Parsing %s" % file)
-
-    #brainwave music file has notes in a flat structure
+    
+    #music file has notes in a single instrument
     notes_to_parse = midi.flat.notes
     
     for element in notes_to_parse:
         #parse notes
-        if isinstance(element, notes_to_parse):
+        if isinstance(element, note.Note):
             notes.append(str(element.pitch))
         #parse chord
         elif isinstance(element, chord.Chord):
             notes.append('.'.join(str(n) for n in element.normalOrder))
 
-    filepath=open('data/seedNotes', 'wb')
+    print(notes)
+    filepath=open('data/notes', 'wb')
     pickle.dump(notes, filepath)
 
     #a list of parsed notes and chord
     return notes
-
-#get_input_notes()
