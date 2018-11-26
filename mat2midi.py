73fa2eb5e85a2814190d59fb867af2267d00a77b
g1121148 <43554058+g1121148@users.noreply.github.com>
2018-11-26 15:26:12 -0500

    Update mat2midi.py

--- mat2midi.py
+++ mat2midi.py
@@ -9,6 +9,11 @@ This moudle converts 2D array from network output into midi file
 	since the input midi for training is actually rescaled when going through midi2mat.py, the input for midi2mat.py and the output for this script are nor exact the same
 	for some reason the output of this script is always played by some weird instruments on my pc, it doesn't sound good from my side. 
 	But this should depend on the software and setting of your midi player
+	
+	-new update with piano_roll2midi function (convert piano roll format output to midi file)
+	-takes np array (piano roll mat),sample rate(fs) and output filename as input
+	-returns midi object and write midi to file
+	
 '''
 
 def mat2midi(mat,name,SampleRate):
@@ -41,6 +46,44 @@ def mat2midi(mat,name,SampleRate):
 		out_midi.writeFile(file)
 	return(out_midi)
 
+
+def piano_roll2midi(mat,outfile,fs):
+	import numpy as np
+	from midiutil.MidiFile import MIDIFile
+	out_midi = MIDIFile(1)
+	track = 0
+	channel = 0
+	time = 0
+	tempo = 120
+	out_midi.addTrackName(track, time, "output track")
+	out_midi.addTempo(track, time, tempo)
+	mat = np.hstack((np.zeros([len(mat),1]),mat,np.zeros([len(mat),1])))
+	delta_vol = np.diff(mat)
+	[a,b] = np.nonzero(delta_vol)
+	for i in range(0,len(a)):
+		n = a[i]
+		m = b[i]
+		if delta_vol[n][m] > 0:
+			pitch = int(n+1)
+			if m > 0 and delta_vol[n][m-1] > 0:
+				vol = int(delta_vol[n][m]+delta_vol[n][m-1])
+			else:
+				vol = int(delta_vol[n][m])
+			tStart = m*1/fs
+		else:
+			if pitch == n+1:
+				tEnd = m*1/fs
+				duration = tEnd-tStart
+			else:
+				pitch = int(n+1)
+				vol = int(0)
+				tStart = m*1/fs
+				duration = 0
+			out_midi.addNote(track, channel, pitch, tStart, duration, vol)
+	filename = "../output/"+outfile+".mid"
+	with open(filename, 'wb') as file:
+	    out_midi.writeFile(file)
+	return(out_midi)
 
 
 
