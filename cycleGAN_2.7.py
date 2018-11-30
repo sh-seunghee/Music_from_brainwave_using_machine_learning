9b468747f4f403baa16cd828cdd67310e961b9d2
SolidShen <43554090+SolidShen@users.noreply.github.com>
2018-11-30 02:31:27 -0500

    Update cycleGAN_2.7.py

--- cycleGAN_2.7.py
+++ cycleGAN_2.7.py
@@ -1,3 +1,41 @@
+'''
+MIT License
+
+Copyright (c) 2018 Erik Linder-Norén
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Modifier: Guangyu Shen
+Last modified: 11/30/2018
+
+This module contains following components
+
+    1.Receive parameters from user on command line
+    2.Build cycle_GAN network instances 
+    3.Train network 
+    4.Sample images in certain batches and save them in dict.
+    5.Print training loss for each epoch
+    6.Save modal parameters 
+    
+'''
+
+
 import argparse
 import os
 import numpy as np
