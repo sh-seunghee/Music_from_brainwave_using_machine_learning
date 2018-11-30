37700ef1474cc2a5345779a069234a241f46a70a
SolidShen <43554090+SolidShen@users.noreply.github.com>
2018-11-30 02:23:37 -0500

    Update model_v27.py

--- model_v27.py
+++ model_v27.py
@@ -1,3 +1,38 @@
+'''
+MIT License
+
+Copyright (c) 2018 Erik Linder-Norén
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Modifier: Guangyu Shen
+Last modified: 11/30/2018
+
+This module contains following components: 
+
+    1.Nornalize input into [0,1]
+    2.Initialize network weights
+    3.Define Generator network structure
+    4.Define Discrinimator network structure
+'''
+
+
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
@@ -19,8 +54,6 @@ def weights_init_normal(m):
         torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
         torch.nn.init.constant_(m.bias.data, 0.0)
 
-def unpack_model(*model):
-    return model
 
 ##############################
 #           RESNET
